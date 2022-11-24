916f7a60c506d73ca0445fd171df3f8898d2af8c
Herbie Vine <vineherbie@gmail.com>
2022-11-24 18:44:26 +0100

    finished address page

--- src/pages/address/[address].tsx
+++ src/pages/address/[address].tsx
@@ -81,6 +81,11 @@ const Address: NextPage<IAddressProps> = () => {
     }
   );
 
+  const handleDonateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") setDonateValue(0);
+    setDonateValue(parseFloat(e.target.value));
+  };
+
   if (loading) return <div>Loading...</div>;
   if (!txs || txs?.length === 0)
     return <div>No transactions found for this address</div>;
@@ -129,7 +134,9 @@ const Address: NextPage<IAddressProps> = () => {
         <div className="w-2/3 h-3/4 flex justify-center items-center">
           <div className="flex flex-col space-y-4">
             <div className="w-1/6 h-4 bg-black" />
-            <h2 className="text-2xl font-black">Donate to SmartB</h2>
+            <h2 className="text-2xl font-black">
+              Donate to SmartB: Project Cookstove
+            </h2>
             <p>
               Increase access of households and communities to improved
               cookstoves by disseminating high thermal efficiency and low
@@ -140,16 +147,45 @@ const Address: NextPage<IAddressProps> = () => {
         </div>
       </div>
       <div className="w-full h-full flex justify-around items-center">
-        <div className="w-2/3 h-3/4 p-8 flex flex-col rounded-xl shadow-md bg-gray-100">
-          <div className="flex justify-between items-center space-x-2">
-            <h1 className="font-black text-lg">Donate to SmartB</h1>
-            {isConnected && <ConnectButton showBalance={true} />}
+        <div className="w-2/3 h-3/4 flex flex-col justify-center items-center space-y-8">
+          <div
+            className={`w-full flex items-center space-x-2 ${
+              isConnected ? "justify-between" : "flex-col"
+            }`}
+          >
+            <h1 className="font-black text-2xl">Donate to SmartB</h1>
+            {isConnected && <ConnectButton showBalance={false} />}
           </div>
-          <div className="w-full h-full flex flex-col justify-center items-center space-y-2">
+          <div className="w-full flex flex-col items-center space-y-4">
             {isConnected ? (
               <>
-                <p>asas</p>
-                <p>asass</p>
+                <div className="w-full flex justify-between items-center">
+                  <p>Donate USDC</p>
+                  <p>
+                    Dept{" "}
+                    {!donateValue
+                      ? 0
+                      : Math.round(
+                          (
+                            (donateValue / (totalEmissions * 0.02)) *
+                            100
+                          ).toFixed(2)
+                        )}
+                    % covered
+                  </p>
+                </div>
+                <input
+                  type="number"
+                  value={donateValue}
+                  onChange={handleDonateChange}
+                  placeholder="Donation Amount in USDC"
+                  className="px-4 py-2 w-full shadow-lg rounded-lg focus:outline-none"
+                />
+                <div className="w-full flex justify-end">
+                  <button className="px-4 py-2 shadow-lg rounded-lg bg-[#0e76fd] text-white focus:outline-none hover:scale-105 duration-75">
+                    Donate {!donateValue ? 0 : donateValue} USDC
+                  </button>
+                </div>
               </>
             ) : (
               <ConnectButton chainStatus="icon" showBalance={false} />
